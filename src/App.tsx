0e45957f84c76f3f218e6ebdf501b2d92e5702c4
Valencio <150917016+asapleejunior@users.noreply.github.com>
2025-12-08 00:32:35 +0300

    Add demo banner to avoid phishing warning

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import Navbar from './components/Layout/Navbar'
 import Sidebar from './components/Layout/Sidebar'
 import Rightbar from './components/Layout/Rightbar'
 import Feed from './components/Feed/Feed'
+import DemoBanner from './components/DemoBanner'
 
 function App() {
   return (
     <div className="min-h-screen bg-facebook-gray">
+      <DemoBanner />
       <Navbar />
       <div className="flex justify-between">
         <Sidebar />
---
+++ src/components/DemoBanner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DemoBanner: React.FC = () => {
+    return (
+        <div className="bg-yellow-400 text-black py-2 px-4 text-center font-semibold text-sm">
+            ⚠️ DEMO PROJECT - This is a Facebook clone created for educational purposes by Valencio
+        </div>
+    );
+};
+
+export default DemoBanner;
